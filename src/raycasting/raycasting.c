1c181b9ff5d96f83c6e81d55246e6d3ec99a1da4
Arthur Urlic <aurlic@student.42.fr>
2024-04-18 14:34:32 +0200

    re arranged functions

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:31:52 by aurlic            #+#    #+#             */
-/*   Updated: 2024/04/17 18:30:46 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/04/18 10:33:17 by aurlic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,10 +95,50 @@ static void	dda_algo(t_game *game, t_player *player, t_ray *ray)
 	}
 }
 
+static void	init_ray(t_game *game, t_player *player, t_ray *ray, int x)
+{
+	double	camera_x; // arrivee du rayon sur axe plane
+
+	camera_x = 2 * x / (double)WIN_W - 1;
+	ray->ray_dir_x = player->dir_x + player->plane_x * camera_x;
+	ray->ray_dir_y = player->dir_y + player->plane_y * camera_x;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+}
+
+static void init_dda(t_game *game, t_player *player, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = ((player->pos_x - ray->map_x) * ray->delta_dist_x);
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = ((ray->map_x + 1 - player->pos_x) * ray->delta_dist_x);
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = ((player->pos_y - ray->map_y) * ray->delta_dist_y);
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = ((ray->map_y + 1 - player->pos_y) * ray->delta_dist_y);
+	}
+}
+
 static void	raycaster(t_game *game, t_player *player, t_ray *ray)
 {
 	int		x; // colonne par colonne pour balayer tout le champ de vision
-	double	camera_x; // arrivee du rayon sur axe plane
 	double	perp_wall_dist; // longueur du rayon qui a touche le mur
 
 	ray->map_x = (int)player->pos_x;
@@ -106,37 +146,8 @@ static void	raycaster(t_game *game, t_player *player, t_ray *ray)
 	x = 0;
 	while (x < WIN_W)
 	{
-		camera_x = 2 * x / (double)WIN_W - 1;
-		ray->ray_dir_x = player->dir_x + player->plane_x * camera_x;
-		ray->ray_dir_y = player->dir_y + player->plane_y * camera_x;
-		if (ray->ray_dir_x == 0)
-			ray->delta_dist_x = 1e30;
-		else
-			ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
-		if (ray->ray_dir_y == 0)
-			ray->delta_dist_y = 1e30;
-		else
-			ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
-		if (ray->ray_dir_x < 0)
-		{
-			ray->step_x = -1;
-			ray->side_dist_x = ((player->pos_x - ray->map_x) * ray->delta_dist_x);
-		}
-		else
-		{
-			ray->step_x = 1;
-			ray->side_dist_x = ((ray->map_x + 1 - player->pos_x) * ray->delta_dist_x);
-		}
-		if (ray->ray_dir_y < 0)
-		{
-			ray->step_y = -1;
-			ray->side_dist_y = ((player->pos_y - ray->map_y) * ray->delta_dist_y);
-		}
-		else
-		{
-			ray->step_y = 1;
-			ray->side_dist_y = ((ray->map_y + 1 - player->pos_y) * ray->delta_dist_y);
-		}
+		init_ray(game, player, ray, x);
+		init_dda(game, player, ray);
 		dda_algo(game, player, ray);
 		x++;  
 	}
